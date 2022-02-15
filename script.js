40459989ec05e95670f2b5d9c91900b0cf26b86a
Yuta0227 <yutahonjo@keio.jp>
2022-02-16 03:18:25 +0900

    入力した数値をカラーコードに代入し、表示することに成功。あとは他の選択肢の色をmathrandomで表示させること

--- script.js
+++ script.js
@@ -3,11 +3,16 @@
 //年月入力時の処理
 const editYear=document.getElementById('edit-year');
 const editMonth=document.getElementById('edit-month');
+const editDate=document.getElementById('edit-date');
 const decideBirth=document.getElementById('decide-birth');
 const choice10=document.getElementById('choice10');
 const choice11=document.getElementById('choice11');
 const choice12=document.getElementById('choice12');
 const answer2=document.getElementById('answer2');
+const shareSection=document.getElementById('share-section');
+var yearText;
+var monthText;
+var dateText;
 var colorCode;
 var j;
 const choice2=[
@@ -15,25 +20,27 @@ const choice2=[
   document.getElementById('choice21'),
   document.getElementById('choice22'),
 ];
+
+
 const decideColor=document.getElementById('decide-color');
 //例としてgreenと緑色表示
 let choiceColor10=choice10.innerHTML;
 choice10.style.backgroundColor=choiceColor10;
 //年月の入力内容を取得
-function checkYearMonth(){
-  let yearText=editYear.innerHTML;
-  let monthText=editMonth.innerHTML;
-  colorCode=yearText+monthText;
-  console.log(colorCode);
+function checkBirth(){
+  yearText=editYear.innerHTML;
+  monthText=editMonth.innerHTML;
+  dateText=editDate.innerHTML;
+  colorCode=`rgb(${yearText}, ${monthText}, ${dateText})`;
 }
 //年月取得を確定時実行
 decideBirth.addEventListener('click',function(){
-  checkYearMonth();
+  checkBirth();
   j=Math.floor(Math.random()*3);
   for(let i=0;i<=2;i++){
-    choice2[i].style.backgroundColor="white";
+    choice2[i].style.backgroundColor="rgb(255,255,255)";
   }
-  choice2[j].style.backgroundColor=`#${colorCode}`;
+  choice2[j].style.backgroundColor=colorCode;
 
 })
 //色の入力内容を取得
@@ -49,15 +56,30 @@ decideColor.addEventListener('click',function(){
 })
 for(let i=0;i<=2;i++){
   choice2[i].addEventListener('click',function(){
-    answer2.innerHTML="こんにちは";//クリック時表示成功
+    console.log(choice2[i].style.backgroundColor);
+    console.log(colorCode);
+    if(choice2[i].style.backgroundColor==colorCode){
+      console.log("ok");
+      answer2.innerHTML="正解!";//クリック時表示成功
+    }else{
+      answer2.innerHTML="不正解!";
+    }
+    shareSection.style.display="flex";
   })
 }
 
 
+window.addEventListener('load',function(){
+  shareSection.style.display="none";//最初は見せず、クリック時に表示
+  document.onkeypress=function(e){
+    if(e.key==='Enter'){
+      return false;
+    }//Enterキー無効化してcolorcodeがずれるのを防ぐ
+  }
+})
 
 
-
-
+//
 
 
 
